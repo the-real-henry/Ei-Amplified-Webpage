5576e8f933a2c002550e2e9824064b6535ac67cd
Henry Fu <henry@eiamplified.com>
2022-05-09 13:14:42 -0400

    Before horizontal Scrolling

--- js/main.js
+++ js/main.js
@@ -4,3 +4,41 @@ collapsibles.forEach((item) =>
     this.classList.toggle("collapsible--expanded");
   })
 );
+
+var navbar = document.getElementById("navbar");
+var menu = document.getElementById("navheight");
+
+window.onscroll = function(){
+    if(window.pageYOffset >= menu.offsetTop){
+        navbar.classList.add("sticky");
+    }
+    else{
+        navbar.classList.remove('sticky');
+    }
+}
+
+
+const faders = documents.querySelectorAll('.fade-in');
+
+const appearOptions = {
+  threshold: 1
+};
+const appearOnScroll = new IntersectionObserver
+(function(
+  entries, 
+  appearOnScroll
+) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add('appear');
+      appearOnScroll.unobserve(entry.target);
+    }
+  })
+}, 
+appearOptions);
+
+faders.forEach(faders => {
+  appearOnScroll.observe(fader);
+})
